53cecc9bcc504f4c9be2abd7d8b0ff868788fde5
Lê Quang Huy <quanghuy789po@gmail.com>
2025-10-26 12:12:18 +0700

    Add files via upload

---
+++ HIlo/HIlo/HashHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HIlo
+{
+    public static class HashHelper
+    {
+        public static string ToSHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }     
+        }
+    }
+}
---
+++ HIlo/HIlo/TCPClientHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace HIlo
+{
+    public class TCPClientHandler
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public bool Connect(string ip, int port)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ip, port);
+                stream = client.GetStream();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Send(string message)
+        {
+            if (stream == null) return;
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+        public string Receive()
+        {
+            if (stream == null) return "";
+            byte[] data = new byte[1024];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            return Encoding.UTF8.GetString(data, 0, bytesRead);
+        }
+
+        public void Close()
+        {
+            stream?.Close();
+            client?.Close();
+        }
+    }
+}

a61ac2386cbb7ade04dd6ade2d13645af63b5270
phuoccouhp <huyphuoc09112005@gmail.com>
2025-10-26 20:27:36 +0700

    tao tcpclient và tcp server
    tao tcpclient và tcp server

---
+++ TCPClient/Dangky.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace BTVN1_Nhom6_NT106.Q12
+{
+    partial class Dangky
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LB_Username = new Label();
+            TB_Username = new TextBox();
+            LB_Email = new Label();
+            LB_Repass = new Label();
+            LB_Pass = new Label();
+            LB_Hoten = new Label();
+            button1 = new Button();
+            TB_Email = new TextBox();
+            TB_Hoten = new TextBox();
+            TB_Pass = new TextBox();
+            TB_Repass = new TextBox();
+            LB_SDT = new Label();
+            LB_NS = new Label();
+            LB_GT = new Label();
+            LB_DC = new Label();
+            TB_SDT = new TextBox();
+            TB_NS = new TextBox();
+            TB_GT = new TextBox();
+            TB_DC = new TextBox();
+            BT_Dangky = new Button();
+            LB_CoTK = new Label();
+            LB_Link = new LinkLabel();
+            SuspendLayout();
+            // 
+            // LB_Username
+            // 
+            LB_Username.AutoSize = true;
+            LB_Username.Font = new Font("Segoe UI", 11F);
+            LB_Username.Location = new Point(50, 38);
+            LB_Username.Name = "LB_Username";
+            LB_Username.Size = new Size(97, 25);
+            LB_Username.TabIndex = 0;
+            LB_Username.Text = "Username";
+            // 
+            // TB_Username
+            // 
+            TB_Username.Location = new Point(212, 36);
+            TB_Username.Name = "TB_Username";
+            TB_Username.Size = new Size(267, 27);
+            TB_Username.TabIndex = 1;
+            // 
+            // LB_Email
+            // 
+            LB_Email.AutoSize = true;
+            LB_Email.Font = new Font("Segoe UI", 11F);
+            LB_Email.Location = new Point(65, 80);
+            LB_Email.Name = "LB_Email";
+            LB_Email.Size = new Size(58, 25);
+            LB_Email.TabIndex = 2;
+            LB_Email.Text = "Email";
+            // 
+            // LB_Repass
+            // 
+            LB_Repass.AutoSize = true;
+            LB_Repass.Font = new Font("Segoe UI", 11F);
+            LB_Repass.Location = new Point(14, 202);
+            LB_Repass.Name = "LB_Repass";
+            LB_Repass.Size = new Size(168, 25);
+            LB_Repass.TabIndex = 3;
+            LB_Repass.Text = "Nhập lại password";
+            // 
+            // LB_Pass
+            // 
+            LB_Pass.AutoSize = true;
+            LB_Pass.Font = new Font("Segoe UI", 11F);
+            LB_Pass.Location = new Point(50, 162);
+            LB_Pass.Name = "LB_Pass";
+            LB_Pass.Size = new Size(91, 25);
+            LB_Pass.TabIndex = 4;
+            LB_Pass.Text = "Password";
+            // 
+            // LB_Hoten
+            // 
+            LB_Hoten.AutoSize = true;
+            LB_Hoten.Font = new Font("Segoe UI", 11F);
+            LB_Hoten.Location = new Point(60, 120);
+            LB_Hoten.Name = "LB_Hoten";
+            LB_Hoten.Size = new Size(70, 25);
+            LB_Hoten.TabIndex = 5;
+            LB_Hoten.Text = "Họ Tên";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(257, 244);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 39);
+            button1.TabIndex = 6;
+            button1.Text = "Tuỳ chọn";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // TB_Email
+            // 
+            TB_Email.Location = new Point(212, 81);
+            TB_Email.Name = "TB_Email";
+            TB_Email.Size = new Size(267, 27);
+            TB_Email.TabIndex = 7;
+            // 
+            // TB_Hoten
+            // 
+            TB_Hoten.Location = new Point(212, 121);
+            TB_Hoten.Name = "TB_Hoten";
+            TB_Hoten.Size = new Size(267, 27);
+            TB_Hoten.TabIndex = 8;
+            // 
+            // TB_Pass
+            // 
+            TB_Pass.Location = new Point(212, 163);
+            TB_Pass.Name = "TB_Pass";
+            TB_Pass.Size = new Size(267, 27);
+            TB_Pass.TabIndex = 9;
+            // 
+            // TB_Repass
+            // 
+            TB_Repass.Location = new Point(212, 202);
+            TB_Repass.Name = "TB_Repass";
+            TB_Repass.Size = new Size(267, 27);
+            TB_Repass.TabIndex = 10;
+            // 
+            // LB_SDT
+            // 
+            LB_SDT.AutoSize = true;
+            LB_SDT.Font = new Font("Segoe UI", 11F);
+            LB_SDT.Location = new Point(36, 297);
+            LB_SDT.Name = "LB_SDT";
+            LB_SDT.Size = new Size(123, 25);
+            LB_SDT.TabIndex = 11;
+            LB_SDT.Text = "Số điện thoại";
+            // 
+            // LB_NS
+            // 
+            LB_NS.AutoSize = true;
+            LB_NS.Font = new Font("Segoe UI", 11F);
+            LB_NS.Location = new Point(48, 339);
+            LB_NS.Name = "LB_NS";
+            LB_NS.Size = new Size(96, 25);
+            LB_NS.TabIndex = 12;
+            LB_NS.Text = "Ngày sinh";
+            // 
+            // LB_GT
+            // 
+            LB_GT.AutoSize = true;
+            LB_GT.Font = new Font("Segoe UI", 11F);
+            LB_GT.Location = new Point(53, 385);
+            LB_GT.Name = "LB_GT";
+            LB_GT.Size = new Size(84, 25);
+            LB_GT.TabIndex = 13;
+            LB_GT.Text = "Giới tính";
+            // 
+            // LB_DC
+            // 
+            LB_DC.AutoSize = true;
+            LB_DC.Font = new Font("Segoe UI", 11F);
+            LB_DC.Location = new Point(58, 427);
+            LB_DC.Name = "LB_DC";
+            LB_DC.Size = new Size(70, 25);
+            LB_DC.TabIndex = 14;
+            LB_DC.Text = "Địa chỉ";
+            // 
+            // TB_SDT
+            // 
+            TB_SDT.Location = new Point(212, 298);
+            TB_SDT.Name = "TB_SDT";
+            TB_SDT.Size = new Size(267, 27);
+            TB_SDT.TabIndex = 15;
+            // 
+            // TB_NS
+            // 
+            TB_NS.Location = new Point(212, 340);
+            TB_NS.Name = "TB_NS";
+            TB_NS.Size = new Size(267, 27);
+            TB_NS.TabIndex = 16;
+            // 
+            // TB_GT
+            // 
+            TB_GT.Location = new Point(212, 386);
+            TB_GT.Name = "TB_GT";
+            TB_GT.Size = new Size(267, 27);
+            TB_GT.TabIndex = 17;
+            // 
+            // TB_DC
+            // 
+            TB_DC.Location = new Point(212, 428);
+            TB_DC.Name = "TB_DC";
+            TB_DC.Size = new Size(267, 27);
+            TB_DC.TabIndex = 18;
+            // 
+            // BT_Dangky
+            // 
+            BT_Dangky.Location = new Point(257, 475);
+            BT_Dangky.Name = "BT_Dangky";
+            BT_Dangky.Size = new Size(121, 39);
+            BT_Dangky.TabIndex = 19;
+            BT_Dangky.Text = "Đăng ký";
+            BT_Dangky.UseVisualStyleBackColor = true;
+            BT_Dangky.Click += BT_Dangky_Click;
+            // 
+            // LB_CoTK
+            // 
+            LB_CoTK.AutoSize = true;
+            LB_CoTK.Font = new Font("Segoe UI", 8F);
+            LB_CoTK.Location = new Point(416, 475);
+            LB_CoTK.Name = "LB_CoTK";
+            LB_CoTK.Size = new Size(136, 19);
+            LB_CoTK.TabIndex = 20;
+            LB_CoTK.Text = "Bạn đã có tài khoản?";
+            // 
+            // LB_Link
+            // 
+            LB_Link.AutoSize = true;
+            LB_Link.Location = new Point(442, 494);
+            LB_Link.Name = "LB_Link";
+            LB_Link.Size = new Size(82, 20);
+            LB_Link.TabIndex = 21;
+            LB_Link.TabStop = true;
+            LB_Link.Text = "Đăng nhập";
+            LB_Link.LinkClicked += LB_Link_LinkClicked;
+            // 
+            // Dangky
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(629, 553);
+            Controls.Add(LB_Link);
+            Controls.Add(LB_CoTK);
+            Controls.Add(BT_Dangky);
+            Controls.Add(TB_DC);
+            Controls.Add(TB_GT);
+            Controls.Add(TB_NS);
+            Controls.Add(TB_SDT);
+            Controls.Add(LB_DC);
+            Controls.Add(LB_GT);
+            Controls.Add(LB_NS);
+            Controls.Add(LB_SDT);
+            Controls.Add(TB_Repass);
+            Controls.Add(TB_Pass);
+            Controls.Add(TB_Hoten);
+            Controls.Add(TB_Email);
+            Controls.Add(button1);
+            Controls.Add(LB_Hoten);
+            Controls.Add(LB_Pass);
+            Controls.Add(LB_Repass);
+            Controls.Add(LB_Email);
+            Controls.Add(TB_Username);
+            Controls.Add(LB_Username);
+            Name = "Dangky";
+            Text = "Dangky";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LB_Username;
+        private TextBox TB_Username;
+        private Label LB_Email;
+        private Label LB_Repass;
+        private Label LB_Pass;
+        private Label LB_Hoten;
+        private Button button1;
+        private TextBox TB_Email;
+        private TextBox TB_Hoten;
+        private TextBox TB_Pass;
+        private TextBox TB_Repass;
+        private Label LB_SDT;
+        private Label LB_NS;
+        private Label LB_GT;
+        private Label LB_DC;
+        private TextBox TB_SDT;
+        private TextBox TB_NS;
+        private TextBox TB_GT;
+        private TextBox TB_DC;
+        private Button BT_Dangky;
+        private Label LB_CoTK;
+        private LinkLabel LB_Link;
+    }
+}
---
+++ TCPClient/Dangky.cs
@@ -0,0 +1,200 @@
+﻿using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+namespace BTVN1_Nhom6_NT106.Q12
+{
+
+
+    public partial class Dangky : Form
+    {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, string lParam);
+        private const int EM_SETCUEBANNER = 0x1501;
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                    builder.Append(b.ToString("x2"));
+                return builder.ToString();
+            }
+        }
+        public Dangky()
+        {
+
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile("Resources/ba32008d4177b9868755336f5e4490f7.jpg");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            LB_Username.BackColor = Color.Transparent;
+            LB_CoTK.BackColor = Color.Transparent;
+            LB_DC.BackColor = Color.Transparent;
+            LB_Email.BackColor = Color.Transparent;
+            LB_GT.BackColor = Color.Transparent;
+            LB_Hoten.BackColor = Color.Transparent;
+            LB_Link.BackColor = Color.Transparent;
+            LB_NS.BackColor = Color.Transparent;
+            LB_Pass.BackColor = Color.Transparent;
+            LB_Repass.BackColor = Color.Transparent;
+            LB_SDT.BackColor = Color.Transparent;
+           
+            this.BT_Dangky.CausesValidation = false;
+            SetCueBanner(TB_Email, "abc@gmail.com");
+            TB_SDT.Visible = false;
+            TB_NS.Visible = false;
+            TB_GT.Visible = false;
+            TB_DC.Visible = false;
+            LB_SDT.Visible = false;
+            LB_NS.Visible = false;
+            LB_GT.Visible = false;
+            LB_DC.Visible = false;
+            TB_Pass.UseSystemPasswordChar = true;
+            TB_Repass.UseSystemPasswordChar = true;
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bool isVisible = TB_SDT.Visible;
+
+            TB_SDT.Visible = !isVisible;
+            TB_NS.Visible = !isVisible;
+            TB_GT.Visible = !isVisible;
+            TB_DC.Visible = !isVisible;
+
+            LB_SDT.Visible = !isVisible;
+            LB_NS.Visible = !isVisible;
+            LB_GT.Visible = !isVisible;
+            LB_DC.Visible = !isVisible;
+        }
+        private void LuuNguoiDungVaoSQL(string username, string email, string hoten, string password,
+                                          string sdt, string ngaysinh, string gioitinh, string diachi)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=QL_TaiKhoan;Integrated Security=True;";
+            string hashedPassword = HashPassword(password);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"INSERT INTO NguoiDung 
+                                (Username, Email, HoTen, PasswordHash, SoDienThoai, NgaySinh, GioiTinh, DiaChi)
+                                 VALUES 
+                                (@Username, @Email, @HoTen, @PasswordHash, @SoDienThoai, @NgaySinh, @GioiTinh, @DiaChi)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@HoTen", string.IsNullOrEmpty(hoten) ? (object)DBNull.Value : hoten);
+                    cmd.Parameters.AddWithValue("@PasswordHash", hashedPassword);
+                    cmd.Parameters.AddWithValue("@SoDienThoai", string.IsNullOrEmpty(sdt) ? (object)DBNull.Value : sdt);
+                    if (DateTime.TryParse(ngaysinh, out DateTime parsedDate))
+                        cmd.Parameters.AddWithValue("@NgaySinh", parsedDate);
+                    else
+                        cmd.Parameters.AddWithValue("@NgaySinh", DBNull.Value);
+
+                    cmd.Parameters.AddWithValue("@GioiTinh", string.IsNullOrEmpty(gioitinh) ? (object)DBNull.Value : gioitinh);
+                    cmd.Parameters.AddWithValue("@DiaChi", string.IsNullOrEmpty(diachi) ? (object)DBNull.Value : diachi);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void BT_Dangky_Click(object sender, EventArgs e)
+        {
+            string username = TB_Username.Text.Trim();
+            string email = TB_Email.Text.Trim();
+            string hoten = TB_Hoten.Text.Trim();
+            string password = TB_Pass.Text;
+            string repassword = TB_Repass.Text;
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show("Không được để username trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(email) || !email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Email phải kết thúc bằng @gmail.com!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(hoten))
+            {
+                MessageBox.Show("Không được để họ tên trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (password.Length < 8)
+            {
+                MessageBox.Show("Mật khẩu phải có ít nhất 8 ký tự!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                MessageBox.Show("Mật khẩu phải chứa ít nhất 1 chữ hoa!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                MessageBox.Show("Mật khẩu phải chứa ít nhất 1 chữ số!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                MessageBox.Show("Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (password != repassword)
+            {
+                MessageBox.Show("Mật khẩu nhập lại không khớp!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string sdt = TB_SDT.Visible ? TB_SDT.Text.Trim() : "";
+            string ngaysinh = TB_NS.Visible ? TB_NS.Text.Trim() : "";
+            string gioitinh = TB_GT.Visible ? TB_GT.Text.Trim() : "";
+            string diachi = TB_DC.Visible ? TB_DC.Text.Trim() : "";
+            try
+            {
+                LuuNguoiDungVaoSQL(username, email, hoten, password, sdt, ngaysinh, gioitinh, diachi);
+                MessageBox.Show("Đăng ký thành công và đã lưu vào cơ sở dữ liệu!",
+                                "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Lỗi khi lưu dữ liệu: "+ ex.Message, "Lỗi SQL",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SetCueBanner(TextBox box, string cue)
+        {
+            SendMessage(box.Handle, EM_SETCUEBANNER, (IntPtr)0, cue);
+        }
+
+       
+        public bool checkmk(string mk)
+        {
+            if (mk == null) return false;
+
+
+            return true;
+        }
+       
+
+        private void LB_Link_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Dangnhap dangnhap= new Dangnhap();
+            dangnhap.Show();
+            this.Close();
+        }
+    }
+}
